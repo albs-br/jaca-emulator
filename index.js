7bfbd6e80103e938b585c24ef1f539796bfb0d10
albs-br <albs_br@yahoo.com.br>
2019-04-05 18:58:11 -0300

    v.0.0.8 - 3 files modified

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 // Import stylesheets
 import './style.css';
 
+//import './jquery.js';
+
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 //appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+
+import * as pJson from "./package.json";
+
+//$('#version').text('v.' + pJson.version);
