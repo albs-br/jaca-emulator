6a56d6aaceab1d4e339e3cca5f17fbac05e8abac
albs-br <albs_br@yahoo.com.br>
2019-04-10 18:03:53 -0300

    v.0.2.1 2 files modified

--- index.js
+++ index.js
@@ -16,7 +16,11 @@ import * as pJson from "./package.json";
 $('#version').text('v.' + pJson.version);
 $('title').text('JACA-2 Emulator v.' + pJson.version);
 
-let memoryTest = '04 00 01\n04 00 FF'
+let memoryTest = 
+  '04 00 0F\n' +  // LD A, 15
+  '05 80 30\n' +  // LD L, 0x30
+  '07 00 FF\n' +  // LD E, 255
+  '08 B0 00'      // LD B, L
 $('#memory').text(memoryTest);
 
 // 0 - Fetch-1
@@ -28,6 +32,7 @@ let pc;
 let ir1;
 let ir2;
 let ir3;
+let registers = new Array(0, 0, 0, 0, 0, 0, 0, 0);
 
 function Reset() {
   cpuState = 0;
@@ -35,7 +40,10 @@ function Reset() {
   ir1 = 0;
   ir2 = 0;
   ir3 = 0;
-  
+  registers.forEach(function (element, index, array) {
+    registers[index] = 0;
+  });
+
   UpdateScreen();
 }
 
@@ -50,6 +58,10 @@ function UpdateScreen() {
   $('#ir3').val(ir3);
 }
 
+function hex2bin(hex){
+    return (parseInt(hex, 16).toString(2)).padStart(8, '0');
+}
+
 $(function () {
   Reset();
 
@@ -57,15 +69,8 @@ $(function () {
     let memory = $('#memory').val().trim();
     let arrMem = memory.replace(/\n/g, ' ').split(' ');
 
-    console.info(arrMem.length);
-    console.info('|' + arrMem[0] + '|');
-
-    // When string is empty, the .split method returns an 
-    // array with one empty string, instead of an empty array
-    if(pc >= arrMem.length || arrMem.length == 1) {
-      console.info('No memory for read at position ' + pc);
-      return;
-    }
+    //console.info(arrMem.length);
+    //console.info('|' + arrMem[0] + '|');
 
     let byteFromMemory = arrMem[pc];
 
@@ -80,8 +85,44 @@ $(function () {
         ir3 = byteFromMemory;
         break;
       case 3:
-        //ir1 = byteFromMemory;
+        // Execute
+        let instruction = hex2bin(ir1) + hex2bin(ir2) + hex2bin(ir3);
+        console.info('instruction: ' + instruction);
+
+        let opcode = parseInt(instruction.substring(0, 6), 2);
+        let r1addr = parseInt(instruction.substring(6, 9), 2);
+        let r2addr = parseInt(instruction.substring(9, 12), 2);
+        let dataValue = parseInt(instruction.substring(16, 24), 2);
+        console.info('opcode: ' + opcode);
+        console.info('r1addr: ' + r1addr);
+        console.info('r2addr: ' + r2addr);
+        console.info('dataValue: ' + dataValue);
+
+        switch(opcode) {
+          case 0:
+            break;
+          case 1:
+            registers[r1addr] = dataValue;
+            break;
+          case 2:
+            registers[r1addr] = registers[r2addr];
+            break;
+        }
+
+        registers.forEach(function (element, index, array) {
+          console.info('register ' + index + ': ' + registers[index]);
+        });
+
         break;
+    }
+
+
+
+    // When string is empty, the .split method returns an 
+    // array with one empty string, instead of an empty array
+    if(pc >= arrMem.length || arrMem.length == 1) {
+      console.info('No memory for read at position ' + pc);
+      return;
     }
 
     if(cpuState != 3) pc++;
